0bdaf06a53521670431643dbe6baaa9ad4b6317a
Pavel Parvez <pavel.cse26@gmail.com>
2020-01-25 22:34:48 +0600

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-//Route::get('/{any}', 'HomeController@index')->where('path', '.*');
-Route::resource('/categories', 'CategoryController');
-Route::resource('/posts', 'PostController');
 
-
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+    //Route::get('/{any}', 'HomeController@index')->where('path', '.*');
+    Route::resource('/categories', 'CategoryController');
+    Route::resource('/posts', 'PostController');
+});
